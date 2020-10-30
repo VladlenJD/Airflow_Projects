2e7b593e669fbcfedab35388912d5f1c9bd3f6e9
VladlenJD <65364893+VladlenJD@users.noreply.github.com>
2020-10-30 17:28:25 +0300

    Updated reading file

--- Project_2_COVID_19_daily_report/COVID_19.py
+++ Project_2_COVID_19_daily_report/COVID_19.py
@@ -175,15 +175,21 @@ def daily_report_to_telegram():
         for i in response.json()['result']:
             if i['message']['chat']['id'] not in new_chats:
                 new_chats.append(i['message']['chat']['id'])
-
-    with open('CHATS_C', 'r') as f:
-        for line in f:
-            chats.append(int(line.strip()))
-    with open('CHATS_C', 'a') as f:
-        for c in new_chats:
-            if c not in chats:
-                chats.append(c)
-                f.write(str(c) + '\n')
+    try:
+        with open('CHATS_C', 'r') as f:
+            for line in f:
+                chats.append(int(line.strip()))
+        with open('CHATS_C', 'a') as f:
+            for c in new_chats:
+                if c not in chats:
+                    chats.append(c)
+                    f.write(str(c) + '\n')
+    except:
+        with open('CHATS_C', 'w') as f:
+            for c in new_chats:
+                if c not in chats:
+                    chats.append(c)
+                    f.write(str(c) + '\n')
                 
     for chat in chats:
         params = {'chat_id': chat, 'text': message}
